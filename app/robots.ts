64a5350474347617b5600d3e83210c8a2cb50813
Jesse Dragstra <jesse.dragstracoll@dowjones.com>
2026-03-18 12:36:18 +0100

    fix: rename sitemap-v1.xml to sitemap-v2.xml and update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ["/api/", "/_next/"],
       },
     ],
-    sitemap: "https://www.manitaspl.com/sitemap-v1.xml",
+    sitemap: "https://www.manitaspl.com/sitemap-v2.xml",
   }
 }
